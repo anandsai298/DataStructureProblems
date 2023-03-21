d96ea11824fc1be894cc6358604280a22e5b69e8
anandsaikumar <anandsai298@gmail.com>
2023-03-21 08:50:20 +0530

    [sai] Add : created the NodeHead class to check nod eis null or not

---
+++ DataStructureProblems/DataStructureProblems/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace DataStructureProblems
+{
+    class program
+    {
+        public static void Main()
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+        }
+    }
+}
