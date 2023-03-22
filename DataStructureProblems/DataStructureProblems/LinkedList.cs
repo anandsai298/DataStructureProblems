92b6b3707407b0412b4947952b18c22609d8e102
anandsaikumar <anandsai298@gmail.com>
2023-03-22 08:38:17 +0530

    [sai] Add : DeleteNodeAtParticularPosition

--- DataStructureProblems/DataStructureProblems/LinkedList.cs
+++ DataStructureProblems/DataStructureProblems/LinkedList.cs
@@ -121,5 +121,29 @@ namespace DataStructureProblems
             }
             return count;
         }
+        public void DeleteNodeAtParticularPosition(int position)
+        {
+            if (this.head==null)
+            {
+                Console.WriteLine("Linked List is empty");
+                return;
+            }
+            Node temp= this.head;
+            if(position==0)
+            {
+                this.head = temp.next;
+                return;
+            }
+            for(int i=0;temp!=null && i<position-1;i++)
+            {
+                temp=temp.next;
+            }
+            if (temp==null || temp.next==null)
+            {
+                return;
+            }
+            Node next = temp.next.next;
+            temp.next = next;
+        }
     }
 }
--- DataStructureProblems/DataStructureProblems/Program.cs
+++ DataStructureProblems/DataStructureProblems/Program.cs
@@ -1,76 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Collections.Generic;
+
 namespace DataStructureProblems
 {
     class program
     {
         public static void Main()
         {
-            bool flag = true;
-            while (flag)
-            {
-                Console.WriteLine("1.Add data\n2.AddReversedata\n3.InsertParticularPosition\n4.RemoveFirstNode\n5.RemoveLastNode\n6.Search");
-                Console.WriteLine("enter option to display");
-                int option=Convert.ToInt32(Console.ReadLine());
-                switch(option) 
-                {
-                    case 1:
-                        LinkedList linkedList = new LinkedList();
-                        linkedList.Add(56);
-                        linkedList.Add(30);
-                        linkedList.Add(70);
-                        break;
-                    case 2:
-                        LinkedList linkedList1 = new LinkedList();
-                        linkedList1.AddReverseOrder(56);
-                        linkedList1.AddReverseOrder(30);
-                        linkedList1.AddReverseOrder(70);
-                        Console.WriteLine("Display reverseorder");
-                        linkedList1.Display();
-                        break;
-                    case 3:
-                        LinkedList linkedList2 = new LinkedList();
-                        linkedList2.Add(56);
-                        linkedList2.Add(70);
-                        linkedList2.Display();
-                        linkedList2.InsertParticularPosition(1, 30);
-                        Console.WriteLine("Display insertion of data");
-                        linkedList2.Display();
-                        break;
-                    case 4:
-                        LinkedList linkedList3 = new LinkedList();
-                        linkedList3.Add(56);
-                        linkedList3.Add(30);
-                        linkedList3.Add(70);
-                        linkedList3.Display();
-                        linkedList3.RemoveFirstNode();
-                        Console.WriteLine("Display after remove of first node data");
-                        linkedList3.Display();
-                        break;
-                    case 5:
-                        LinkedList linkedList4 = new LinkedList();
-                        linkedList4.Add(56);
-                        linkedList4.Add(30);
-                        linkedList4.Add(70);
-                        linkedList4.Display();
-                        linkedList4.RemoveLastNode();
-                        Console.WriteLine("Display after remove of last node data");
-                        linkedList4.Display();
-                        break;
-                    case 6:
-                        LinkedList linkedList5 = new LinkedList();
-                        linkedList5.Add(56);
-                        linkedList5.Add(30);
-                        linkedList5.Add(70);
-                        linkedList5.Display();
-                        Console.WriteLine("Display search data of 30");
-                        int position = linkedList5.Search(30);
-                        Console.WriteLine("Display insert data after 30 is 40");
-                        linkedList5.InsertParticularPosition(position+1, 40);
-                        linkedList5.Display();
-                        break;
-                }
-            }   
-        }
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+            Console.WriteLine("Display search data of 30");
+            int position = linkedList.Search(30);
+            Console.WriteLine("Display insert data after 30 is 40");
+            linkedList.InsertParticularPosition(position + 1, 40);
+            linkedList.Display();
+            int searchDelete = linkedList.Search(40);
+            linkedList.DeleteNodeAtParticularPosition(searchDelete);
+            linkedList.Display();
+        }   
     }
 }
