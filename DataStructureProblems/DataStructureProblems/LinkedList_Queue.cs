c240b74e3a204b84cadb505bee051b5d848c5734
anandsaikumar <anandsai298@gmail.com>
2023-03-22 21:09:17 +0530

    [sai] Add : LinkedList_Queue_Enqueue

---
+++ DataStructureProblems/DataStructureProblems/LinkedList_Queue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblems
+{
+    public class LinkedList_Queue
+    {
+        public Node head;
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into Queue", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- DataStructureProblems/DataStructureProblems/Program.cs
+++ DataStructureProblems/DataStructureProblems/Program.cs
@@ -8,28 +8,49 @@ namespace DataStructureProblems
     {
         public static void Main()
         {
-            //LinkedList linkedList = new LinkedList();
-            //linkedList.Add(56);
-            //linkedList.Add(30);
-            //linkedList.Add(70);
-            //linkedList.Display();
-            //Console.WriteLine("Display position of search data 30 is");
-            //int position = linkedList.Search(30);
-            //Console.WriteLine("Display insert data after 30 is 40");
-            //linkedList.InsertParticularPosition(position + 1, 40);
-            //linkedList.Display();
-            //Console.WriteLine("Display position of search data 40 is");
-            //int searchDelete = linkedList.Search(40);
-            //linkedList.DeleteNodeAtParticularPosition(searchDelete);
-            //linkedList.Display();
-            LinkedList_Stack stack = new LinkedList_Stack();
-            stack.Push(70);
-            stack.Push(30);
-            stack.Push(50);
-            stack.Display();
-            stack.Peek();
-            stack.Pop();
-            stack.Display();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.LinkedList\n2.LinkedListStack\n3.LinkedListQueue");
+                Console.WriteLine("enter option to display");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        LinkedList linkedList = new LinkedList();
+                        linkedList.Add(56);
+                        linkedList.Add(30);
+                        linkedList.Add(70);
+                        linkedList.Display();
+                        Console.WriteLine("Display position of search data 30 is");
+                        int position = linkedList.Search(30);
+                        Console.WriteLine("Display insert data after 30 is 40");
+                        linkedList.InsertParticularPosition(position + 1, 40);
+                        linkedList.Display();
+                        Console.WriteLine("Display position of search data 40 is");
+                        int searchDelete = linkedList.Search(40);
+                        linkedList.DeleteNodeAtParticularPosition(searchDelete);
+                        linkedList.Display();
+                        break;
+                    case 2:
+                        LinkedList_Stack stack = new LinkedList_Stack();
+                        stack.Push(70);
+                        stack.Push(30);
+                        stack.Push(50);
+                        stack.Display();
+                        stack.Peek();
+                        stack.Pop();
+                        stack.Display();
+                        break;
+                    case 3:
+                        LinkedList_Queue queue = new LinkedList_Queue();
+                        queue.Enqueue(50);
+                        queue.Enqueue(30);
+                        queue.Enqueue(70);
+                        queue.Display();
+                        break;
+                }
+            }
         }
     }
 }
